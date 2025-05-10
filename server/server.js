41c5168b5619d65176fe259ef3b9cc79bbb79f35
Husam Al-Dhaheri <hossamaldahry@gmail.com>
2025-05-10 23:51:10 +0300

    fix: update CORS configuration to allow multiple origins for frontend access

--- server/server.js
+++ server/server.js
@@ -17,7 +17,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors({
-  origin:"http://localhost:5173" ||  process.env.FRONTEND_URL , // لتسمح بالفرونت من Vercel
+ origin: [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL
+],
   credentials: true
 }));
 
